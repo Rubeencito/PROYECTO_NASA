826f8f41d2d3846a7b253d4d9ef9dd297ed15fc9
Rubeencito <ruben.huertas@institutvidreres.cat>
2023-09-14 14:33:47 +0200

    inicio de session

--- src/main/java/com/example/nasa/inicio_session.java
+++ src/main/java/com/example/nasa/inicio_session.java
@@ -1,11 +1,13 @@
 package com.example.nasa;
 
-//hola
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class inicio_session {
 
@@ -17,44 +19,88 @@ public class inicio_session {
     private JButton iniciarSesionButton;
 
     public inicio_session() {
-        // Crear el frame principal
-        frame = new JFrame("Inicio de Sesión");
+        // Crear un frame sin decoración
+        frame = new JFrame();
+        frame.setUndecorated(true); // Sin bordes de ventana
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 200);
 
-        // Crear el panel principal
-        panel = new JPanel() {
-            // Override para pintar la imagen de fondo
+        // Obtener el tamaño de la pantalla
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        // Establecer el tamaño y la posición manualmente (ajustar según sea necesario)
+        frame.setSize(screenSize.width, screenSize.height);
+        frame.setLocation(0, 0);
+
+        // Crear un panel para el fondo de la imagen con el mismo tamaño que la pantalla
+        JPanel backgroundPanel = new JPanel() {
+            private BufferedImage image;
+
+            {
+                try {
+                    // Carga la imagen de fondo desde el archivo "fondo.jpg"
+                    File imageFile = new File("src/main/java/com/example/nasa/fondo.jpg");
+                    image = ImageIO.read(imageFile);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                ImageIcon imagenFondo = new ImageIcon(System.getProperty("user.home") + "/Desktop/fondo.jpg"); // Ruta de la imagen en el escritorio
-                Image image = imagenFondo.getImage();
-                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+                if (image != null) {
+                    g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+                }
             }
         };
-        panel.setLayout(new GridLayout(4, 2));
+        backgroundPanel.setLayout(new GridBagLayout());
+
+        // Crear el panel principal para los componentes
+        panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(30, 30, 30, 30); // Espacio entre componentes (más grande)
+
+        // Crear el título "NASA" y centrarlo en el recuadro
+        JLabel titleLabel = new JLabel("NASA");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 48)); // Fuente y tamaño personalizado
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2; // Ocupa dos columnas
+        gbc.anchor = GridBagConstraints.CENTER; // Centra el título en el recuadro
+        panel.add(titleLabel, gbc);
+
+        // Crear los campos de usuario y contraseña a la izquierda del cuadro de texto
+        usuarioField = new JTextField(20); // Hacer el campo más ancho
+        contrasenaField = new JPasswordField(20); // Hacer el campo más ancho
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1; // Restablecer a una columna
+        gbc.anchor = GridBagConstraints.WEST; // Coloca los campos a la izquierda
+        panel.add(new JLabel("Usuario:"), gbc);
+        gbc.gridx = 1;
+        panel.add(usuarioField, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        panel.add(new JLabel("Contraseña:"), gbc);
+        gbc.gridx = 1;
+        panel.add(contrasenaField, gbc);
 
         // Crear el desplegable de roles
         String[] roles = {"Físico", "Astronauta", "Espía", "Mecánico"};
         rolesComboBox = new JComboBox<>(roles);
-
-        // Crear los campos de usuario y contraseña
-        usuarioField = new JTextField(10);
-        contrasenaField = new JPasswordField(10);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 2; // Ocupa dos columnas
+        gbc.anchor = GridBagConstraints.CENTER; // Centra el desplegable en el recuadro
+        panel.add(rolesComboBox, gbc);
 
         // Crear el botón de inicio de sesión
         iniciarSesionButton = new JButton("Iniciar Sesión");
-
-        // Agregar componentes al panel
-        panel.add(new JLabel("Rol:"));
-        panel.add(rolesComboBox);
-        panel.add(new JLabel("Usuario:"));
-        panel.add(usuarioField);
-        panel.add(new JLabel("Contraseña:"));
-        panel.add(contrasenaField);
-        panel.add(new JLabel()); // Espacio en blanco
-        panel.add(iniciarSesionButton);
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 2; // Ocupa dos columnas
+        panel.add(iniciarSesionButton, gbc);
 
         // Agregar acción al botón de inicio de sesión
         iniciarSesionButton.addActionListener(new ActionListener() {
@@ -72,8 +118,15 @@ public class inicio_session {
             }
         });
 
-        // Agregar el panel al frame y hacerlo visible
-        frame.add(panel);
+        // Agregar el panel principal al panel de fondo
+        GridBagConstraints bgc = new GridBagConstraints();
+        bgc.anchor = GridBagConstraints.CENTER;
+        backgroundPanel.add(panel, bgc);
+
+        // Agregar el panel de fondo al frame
+        frame.add(backgroundPanel);
+
+        // Hacer el frame visible
         frame.setVisible(true);
     }
 
