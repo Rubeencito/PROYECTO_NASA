9db60c2fd6e095dc7134a88514a87c85c738e5d0
PauCaMu <pau.casas@institutvidreres.cat>
2023-09-21 09:42:17 +0200

    ASTRONAUTA

--- src/main/java/com/example/nasa/ConexionDB.java
+++ src/main/java/com/example/nasa/ConexionDB.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class ConexionDB {
     private static final String URL = "jdbc:mysql://localhost:3306/nasa";
     private static final String USUARIO = "root";
-    private static final String CONTRASENA = "Admin123";
+    private static final String CONTRASENA = "admin123";
 
     public static Connection obtenerConexion() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, CONTRASENA);
--- src/main/java/com/example/nasa/pag_astronauta.java
+++ src/main/java/com/example/nasa/pag_astronauta.java
@@ -2,91 +2,129 @@ package com.example.nasa;
 
 import javax.swing.*;
 import java.awt.*;
-import java.sql.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class pag_astronauta extends JFrame {
 
+    private JLabel astronautLabel;
+
     public pag_astronauta(String nombreUsuario) {
-        // Configura la ventana
         setTitle("Página del Astronauta");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 300); // Establece el tamaño de la ventana
-        setLocationRelativeTo(null); // Centra la ventana en la pantalla
+        setSize(800, 600);
+        setLocationRelativeTo(null);
 
-        // Crea un panel principal con GridLayout
-        JPanel panel = new JPanel(new GridLayout(1, 2));
+        JPanel mainPanel = new JPanel(new BorderLayout());
 
-        try {
-            // Conecta a la base de datos
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/NASA", "root", "admin123");
-            Statement stmt = conn.createStatement();
+        // Crear un panel para la izquierda con fondo gris claro
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBackground(new Color(200, 200, 200));
+        leftPanel.setPreferredSize(new Dimension(100, getHeight()));
+        mainPanel.add(leftPanel, BorderLayout.WEST);
 
-            // Obtén los datos del astronauta a partir del nombre de usuario
-            String query = "SELECT * FROM Astronauta INNER JOIN Usuario ON Astronauta.ID = Usuario.ID WHERE nombreUsuario = '" + nombreUsuario + "'";
-            ResultSet rs = stmt.executeQuery(query);
+        // Crear un panel para la derecha con fondo gris claro
+        JPanel rightPanel = new JPanel();
+        rightPanel.setBackground(new Color(200, 200, 200));
+        rightPanel.setPreferredSize(new Dimension(100, getHeight()));
+        mainPanel.add(rightPanel, BorderLayout.EAST);
 
-            // Procesa los resultados y muestra los datos
-            if (rs.next()) {
-                // Panel para mostrar los datos a la izquierda
-                JPanel dataPanel = new JPanel(new GridLayout(7, 2));
-                dataPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        JPanel centerPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 10;
+        gbc.weighty = 10;
+        gbc.anchor = GridBagConstraints.NORTHWEST;  // Alineación en la esquina superior izquierda
 
-                dataPanel.add(new JLabel("Nombre:"));
-                dataPanel.add(new JLabel(rs.getString("nom")));
+        // Agregar texto "COORDENADAS" con formato
+        JLabel coordenadasLabel = new JLabel("COORDENADAS");
+        coordenadasLabel.setFont(new Font("Arial", Font.BOLD, 24));
 
-                dataPanel.add(new JLabel("Edad primer vuelo:"));
-                dataPanel.add(new JLabel(String.valueOf(rs.getInt("edat_primer_vol"))));
+        // Agregar espacio entre el texto y el botón
+        gbc.insets = new Insets(10, 0, 0, 0);
+        centerPanel.add(coordenadasLabel, gbc);
 
-                dataPanel.add(new JLabel("Edad:"));
-                dataPanel.add(new JLabel(String.valueOf(rs.getInt("edat"))));
-
-                dataPanel.add(new JLabel("Misiones exitosas:"));
-                dataPanel.add(new JLabel(String.valueOf(rs.getInt("missions_ok"))));
-
-                dataPanel.add(new JLabel("Misiones fallidas:"));
-                dataPanel.add(new JLabel(String.valueOf(rs.getInt("missions_ko"))));
-
-                dataPanel.add(new JLabel("Dirección:"));
-                dataPanel.add(new JLabel(rs.getString("adreça")));
-
-                dataPanel.add(new JLabel("Rango militar:"));
-                dataPanel.add(new JLabel(rs.getString("rang_militar")));
-
-                // Agregar el panel de datos a la izquierda
-                panel.add(dataPanel);
-
-                // Agregar imagen de astronauta en la parte derecha
-                ImageIcon astronautIcon = new ImageIcon("C:\\Users\\pauca\\IdeaProjects\\PROYECTO_NASA_\\src\\main\\java\\com\\example\\nasa\\astronauta.png");
-                JLabel astronautLabel = new JLabel(astronautIcon);
-                astronautLabel.setHorizontalAlignment(SwingConstants.CENTER);
-                panel.add(astronautLabel);
-            } else {
-                // Si no se encuentra el astronauta
-                JLabel noDataLabel = new JLabel("No se encontraron datos para este usuario.");
-                noDataLabel.setHorizontalAlignment(SwingConstants.CENTER);
-                panel.add(noDataLabel);
+        JButton mostrarDireccionButton = new JButton("Enviar coordenadas");
+        mostrarDireccionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mostrarDireccionAleatoria();
             }
+        });
 
-            // Cierra la conexión y la consulta
-            rs.close();
-            stmt.close();
-            conn.close();
+        gbc.gridx = 1;  // Cambio de posición en la cuadrícula
+        gbc.insets = new Insets(30, 30, 0, 30);
+        centerPanel.add(mostrarDireccionButton, gbc);
 
-        } catch (SQLException e) {
+        // Agregar imagen a la derecha del astronauta
+        try {
+            ImageIcon astronautIconRight = new ImageIcon("C:\\Users\\pauca\\IdeaProjects\\PROYECTO_NASA_\\src\\main\\java\\com\\example\\nasa\\astronauta.png");
+            int imageWidthRight = astronautIconRight.getIconWidth() / 2;  // Cambiar el divisor para aumentar el tamaño
+            int imageHeightRight = astronautIconRight.getIconHeight() / 2; // Cambiar el divisor para aumentar el tamaño
+            astronautIconRight.setImage(astronautIconRight.getImage().getScaledInstance(imageWidthRight, imageHeightRight, Image.SCALE_DEFAULT)); // Escalar la imagen
+            astronautLabel = new JLabel(astronautIconRight);
+            astronautLabel.setBounds(60, 90, imageWidthRight, imageHeightRight);
+            rightPanel.add(astronautLabel);
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        // Agrega el panel a la ventana
-        add(panel);
 
-        // Hace visible la ventana
+        // Agregar imagen a la izquierda del astronauta
+        try {
+            ImageIcon astronautIconLeft = new ImageIcon("C:\\Users\\pauca\\IdeaProjects\\PROYECTO_NASA_\\src\\main\\java\\com\\example\\nasa\\astronauta.png");
+            int imageWidthLeft = astronautIconLeft.getIconWidth() / 2;  // Cambiar el divisor para aumentar el tamaño
+            int imageHeightLeft = astronautIconLeft.getIconHeight() / 2; // Cambiar el divisor para aumentar el tamaño
+            astronautIconLeft.setImage(astronautIconLeft.getImage().getScaledInstance(imageWidthLeft, imageHeightLeft, Image.SCALE_DEFAULT)); // Escalar la imagen
+            JLabel astronautLabelLeft = new JLabel(astronautIconLeft);
+            astronautLabelLeft.setBounds(60, 90, imageWidthLeft, imageHeightLeft);
+            leftPanel.add(astronautLabelLeft);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Panel para el espacio arriba del texto y el botón
+        JPanel topSpacePanel = new JPanel();
+        topSpacePanel.setBackground(new Color(200, 200, 200));
+        topSpacePanel.setPreferredSize(new Dimension(getWidth(), 20));
+        mainPanel.add(topSpacePanel, BorderLayout.NORTH);
+
+        // Panel para el espacio debajo del texto y el botón
+        JPanel bottomSpacePanel = new JPanel();
+        bottomSpacePanel.setBackground(new Color(200, 200, 200));
+        bottomSpacePanel.setPreferredSize(new Dimension(getWidth(), 20));
+        mainPanel.add(bottomSpacePanel, BorderLayout.SOUTH);
+
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+
+        add(mainPanel);
         setVisible(true);
     }
 
+    private void mostrarDireccionAleatoria() {
+        Random random = new Random();
+        int latitud = random.nextInt(90);
+        int longitud = random.nextInt(180);
+
+        JOptionPane.showMessageDialog(this, "Coordenadas: Latitud " + latitud + ", Longitud " + longitud);
+    }
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            // Modifica "Nombre de Usuario" con el nombre real de usuario que deseas mostrar
-            new pag_astronauta("Nombre de Usuario");
+        JButton iniciarSesionButton = new JButton("Iniciar Sesión");
+        iniciarSesionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new pag_astronauta("Nombre de Usuario");
+            }
         });
+
+        JFrame loginFrame = new JFrame("Iniciar Sesión");
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginFrame.setSize(300, 200);
+        loginFrame.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 20));  // Alineación izquierda
+        loginFrame.add(iniciarSesionButton);
+        loginFrame.setVisible(true);
     }
 }
