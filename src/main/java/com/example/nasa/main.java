53e6345631c0ed1dfb2d9930a965514b544285f8
Nere <Nereaberdugo2002@gmail.com>
2023-09-21 08:06:25 +0200

    comit 4

--- src/main/java/com/example/nasa/main.java
+++ src/main/java/com/example/nasa/main.java
@@ -5,7 +5,7 @@ public class main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-               // new inicio_session();
+               new inicio_session();
                 String usuario = null;
                 pag_mecanico ventana = new pag_mecanico(usuario);
                 ventana.setVisible(true);
